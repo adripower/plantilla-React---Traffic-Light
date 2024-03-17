358ef2428abb5ef25641a6af9edc4a0d1bec9588
adripower <adriantorres.gc@gmail.com>
2024-03-17 21:34:54 +0000

    new file:   src/styles/index.css 	new file:   template.html 	new file:   webpack.config.js

---
+++ src/js/component/home.jsx
@@ -0,0 +1,32 @@
+import React,{useState} from "react";  // se importa el hook useState
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Home = () => {
+//funcionalidades
+const[luz,setLuz]=useState("verde");
+
+
+
+	return (
+		<div className="text-center">
+			<div  className=" semaforo ">
+				<div
+          		className={`rojo ${luz === "rojo" ? "rojoF" : ""}`}
+				onClick={() => setLuz("rojo")}>
+				</div>
+				<div className={`naranja ${luz === "naranja" ? "naranjaF" : ""}`}
+        		onClick={() => setLuz("naranja")}>
+				</div>
+				<div className={`verde ${luz === "verde" ? "verdeF" : ""}`}
+        		onClick={() => setLuz("verde")}>
+		 		</div>
+						</div>
+			
+		</div>
+	);
+};
+
+export default Home;
